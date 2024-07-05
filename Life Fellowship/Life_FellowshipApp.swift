9c5789bc226313049d0d8873e3fbaab561b8f321
Blake Lawson <blawson490@gmail.com>
2024-07-05 14:25:20 -0500

    Initial Commit

---
+++ Life Fellowship/Life_FellowshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_FellowshipApp.swift
+//  Life Fellowship
+//
+//  Created by Blake Lawson on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Life_FellowshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
